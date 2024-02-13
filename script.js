4c61f30751766d77431e37d9940fa33fcfbcf5eb
Dan Turner <55895288+thedanturner@users.noreply.github.com>
2024-02-13 07:08:12 +0000

    Added additional backgrounds

--- script.js
+++ script.js
@@ -1,7 +1,6 @@
 const apiKey = 'd8ab63bd9e5758217288b4ee51244e67';
 const apiUrl = 'https://api.openweathermap.org/data/2.5/weather';
 
-
 const locationInput = document.getElementById('locationInput');
 const searchButton = document.getElementById('searchButton');
 const locationElement = document.getElementById('location');
@@ -35,9 +34,18 @@ function fetchWeather(location) {
             mainElement.textContent = data.weather[0].main;
             descriptionElement.textContent = data.weather[0].description;
             iconElement.src = `http://openweathermap.org/img/wn/${data.weather[0].icon}.png`;
+
+            if (data.weather[0].main === 'Clouds') {
+                document.body.style.backgroundImage = 'url(https://images.freeimages.com/images/large-previews/294/partly-cloudy-1173077.jpg)';
+            } else if (data.weather[0].main === 'Rain') {
+                document.body.style.backgroundImage = 'url(https://images.freeimages.com/images/large-previews/9f6/rain-1381186.jpg)';
+            } else {
+                document.body.style.backgroundImage = 'url(https://www.saga.co.uk/contentlibrary/saga/publishing/verticals/technology/apps/shutterstock_240459751.jpg)';
+            }
         })
         .catch(error => {
             console.error('Error fetching weather data:', error);
             locationElement.textContent = 'Error fetching weather data';
         });
-}
+    }
+
