315d22ec2582170e3f66dcba9be4ccc464e403b8
Dan Turner <55895288+thedanturner@users.noreply.github.com>
2024-02-17 19:17:30 +0000

    Added URLs to background weather images

--- script.js
+++ script.js
@@ -70,19 +70,29 @@ async function fetchWeather(location) {
         }
         const data = await response.json();
         // Update your UI elements here
-        locationElement.textContent = data.name;
-        temperatureElement.textContent = `${Math.round(data.main.temp)}°C`;
-        mainElement.textContent = data.weather[0].main;
-        descriptionElement.textContent = data.weather[0].description;
-        iconElement.src = `http://openweathermap.org/img/wn/${data.weather[0].icon}.png`;
- 
-        // Background image logic
-        if (data.weather[0].main === 'Clouds') {
-            document.body.style.backgroundImage = 'url(https://images.freeimages.com/images/large-previews/294/partly-cloudy-1173077.jpg)';
-        } // Add other conditions here
- 
-        return data; // This will still return a Promise, but you can use await when calling fetchWeather
-    } catch (error) {
+            locationElement.textContent = data.name;
+            temperatureElement.textContent = `${Math.round(data.main.temp)}°C`;
+            mainElement.textContent = data.weather[0].main;
+            descriptionElement.textContent = data.weather[0].description;
+            iconElement.src = `http://openweathermap.org/img/wn/${data.weather[0].icon}.png`;
+     
+            // Background image logic
+            if (data.weather[0].main === 'Clouds') {
+                document.body.style.backgroundImage = 'url(https://images.freeimages.com/images/large-previews/294/partly-cloudy-1173077.jpg)';
+            } else if (data.weather[0].main === 'Rain') {
+                document.body.style.backgroundImage = 'url(https://www.publicdomainpictures.net/pictures/70000/velka/background-with-rain.jpg)';
+            } else if (data.weather[0].main === 'Clear') {
+                document.body.style.backgroundImage = 'url(https://www.photos-public-domain.com/wp-content/uploads/2011/02/bright-sun-in-blue-sky.jpg)';  
+            } else if (data.weather[0].main === 'Snow') {
+                document.body.style.backgroundImage = 'url(https://dm0qx8t0i9gc9.cloudfront.net/thumbnails/video/rZJIMvhmliwmde8a6/videoblocks-snowfall-on-the-background-of-blurred-forest-slow-motion-snow-falling-down-against-blurred-background-winter-holidays-season_szhypw86q_thumbnail-1080_01.png)'; 
+            } else if (data.weather[0].main === 'Mist') {
+                document.body.style.backgroundImage = 'url(https://freebigpictures.com/wp-content/uploads/2009/09/mist.jpg)'; 
+            } else {
+                document.body.style.backgroundImage = 'url(https://www.saga.co.uk/contentlibrary/saga/publishing/verticals/technology/apps/shutterstock_240459751.jpg)';
+            }
+     
+            return data; // This will still return a Promise, but you can use await when calling fetchWeather
+        } catch (error) {
         console.error('Error fetching weather data:', error);
         locationElement.textContent = 'Error fetching weather data';
         throw error; // This allows the error to be caught by the caller of fetchWeather
